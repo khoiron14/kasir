a72563fe617f882ddd6880f24043d9920a850651
khoiron <khoironarkade@gmail.com>
2019-02-19 19:17:49 +0700

    menambah fungsi relasi User dan Transaction

--- app/Transaction.php
+++ app/Transaction.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Transaction extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function user() {
+    	return $this->belongsTo(User::class);
+    }
 }
--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use App\Transaction;
 
 class User extends Authenticatable
 {
@@ -27,4 +28,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function transactions() {
+        return $this->hasMany(Transaction::class);
+    }
 }

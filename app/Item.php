1105a126363bd2217646049deef014c9088b691d
khoiron <khoironarkade@gmail.com>
2019-02-19 19:14:31 +0700

    menambah fungsi relasi Item dan Cart

--- app/Item.php
+++ app/Item.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\ItemCategory as Category;
+use App\Cart;
 
 class Item extends Model
 {
@@ -11,5 +12,9 @@ class Item extends Model
 
     public function category() {
     	return $this->belongsTo(Category::class);
+    }
+
+    public function cart() {
+    	return $this->hasOne(Cart::class);
     }
 }

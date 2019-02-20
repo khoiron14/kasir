ea56c9c1d62500398520d18b695c30effce02a71
khoiron <khoironarkade@gmail.com>
2019-02-20 22:24:57 +0700

    mengirim data Item ke '/'

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Item;
 
 class HomeController extends Controller
 {
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $items = Item::where('stock', '>', 0)->orderBy('name')->get();
+
+        return view('home', compact('items'));
     }
 }

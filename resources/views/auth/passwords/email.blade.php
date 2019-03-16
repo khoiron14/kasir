8bf64531512002c8a5157552326b83c814567c53
arifinizz <arifin.izzah15@gmail.com>
2019-03-16 22:08:16 +0700

    change password reset page with bulma

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,47 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
+<section class="hero is-fullheight is-light">
+    <div class="hero-body">
+        <div class="container">
+            <center>
+                <div class="box" style="width: 500px">
+                    <!--Reset Password-->
+                    <h3 class="title is-3">{{ __('Reset Password') }}</h3>
+                    <br>
                     @if (session('status'))
-                        <div class="alert alert-success" role="alert">
+                        <div class="help is-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
-
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
+                        <div class="field">
+                            <p class="control has-icons-left has-icons-right">
+                                <input id="email" type="email" class="input is-rounded is-medium{{ $errors->has('email') ? ' is-danger' : '' }}" placeholder="{{ __('E-Mail Address') }}" name="email" value="{{ old('email') }}" required>
+                                <span class="icon is-small is-left">
+                                    <i class="fas fa-envelope"></i>
+                                </span>
                                 @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+                                    <p class="help is-danger" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
+                                    </p>
                                 @endif
+                                </p>
+                        </div>
+                        <br>
+                        <div class="field">
+                            <div class="control">
+                                <button class="button is-medium is-info is-fullwidth is-rounded" type="submit">{{ __('Send Password Reset Link') }}</button>
                             </div>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
+                        <a class="button is-text is-rounded" href="{{ route('login') }}">{{ __('Kembali ke halaman login') }}</a>
                     </form>
                 </div>
-            </div>
+            </center>
         </div>
     </div>
-</div>
+</section>
 @endsection

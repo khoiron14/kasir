d75e868be59b3911875cfc1ec5a89a44bc23212f
arifinizz <arifin.izzah15@gmail.com>
2019-03-16 21:44:17 +0700

    update login & add register with bulma

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,10 +6,12 @@
         <div class="container">
             <center>
                 <div class="box" style="width: 500px">
-                    <h3 class="title is-3">Login</h3>
+                    <!--Login-->
+                    <h3 class="title is-3">{{ __('Login') }}</h3>
                     <br>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        <!--Input email-->
                         <div class="field">
                             <p class="control has-icons-left has-icons-right">
                                 <input id="email" type="email" class="input is-rounded is-medium{{ $errors->has('email') ? ' is-danger' : '' }}" placeholder="{{ __('E-Mail Address') }}" name="email" value="{{ old('email') }}" required autofocus>
@@ -23,6 +25,7 @@
                                 @endif
                             </p>
                         </div>
+                        <!--Input password-->
                         <div class="field">
                             <p class="control has-icons-left has-icons-right">
                                 <input id="password" type="password" class="input is-rounded is-medium{{ $errors->has('password') ? ' is-danger' : '' }}" placeholder="{{ __('Password') }}" name="password" required>
@@ -36,6 +39,7 @@
                                 @endif
                             </p>
                         </div>
+                        <!--Remember me-->
                         <div class="field">
                             <div class="control">
                                 <label class="checkbox" for="remember">
@@ -44,22 +48,23 @@
                                 </label>
                             </div>
                         </div>
+                        <!--Submit button-->
                         <div class="field">
                             <div class="control">
                                 <button class="button is-medium is-info is-fullwidth is-rounded" type="submit">{{ __('Login') }}</button>
                             </div>
                         </div>
+                        <!--Register button-->
                         <div class="field">
                             <div class="control">
                                 <a class="button is-medium is-light is-fullwidth is-rounded" href="{{ route('register') }}">{{ __('Register') }}</a>
                             </div>
                         </div>
                         <br>
+                        <!--Forgot password-->
                         <div class="is-divider">
                             @if (Route::has('password.request'))
-                                <a class="button is-text is-rounded" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
+                                <a class="button is-text is-rounded" href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
                             @endif
                         </div>
                     </form>
